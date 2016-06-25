971f6325c4595f2ab8927a75c8ee2398c190a6b1
Simon Katz <sk@nomistech.com>
2016-06-25 15:20:24 +0100

    More playing with Quil

--- src/cljs/nomisdraw/quil_play.cljs
+++ src/cljs/nomisdraw/quil_play.cljs
@@ -21,12 +21,25 @@
           (update-state [state]
             (update state :time inc))
           (draw [state]
-            (q/background 255)
-            (q/fill 0)
-            (q/ellipse (rem (:time state) w)
-                       (rem (:time state) h)
-                       55
-                       55))]
+            (let [t (:time state)]
+              (q/background 255)
+              (q/fill 255)
+              (q/ellipse (rem t w)
+                         (rem t h)
+                         55
+                         55)
+              (q/fill 0)
+              (let [tt (* t 100)
+                    tt (* (rem tt 10000) 0.01)]
+                (q/with-translation [(/ w 2) (/ h 2)]
+                  (q/ellipse (* tt (q/sin tt))
+                             (* tt (q/cos tt))
+                             10
+                             10)))
+              (q/with-translation [(/ w 2) (/ h 2)]
+                (doseq [t (range 0 100 0.01)]
+                  (q/point (* t (q/sin t))
+                           (* t (q/cos t)))))))]
     ;; FIXME:
     ;; I had `q/sketch` here, but it doesn't work on browser refresh.
     ;; It's OK on a Figwheel reload, though.
