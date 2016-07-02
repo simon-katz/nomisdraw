ebe528af90820d93205c7244029a519b72f38315
Simon Katz <sk@nomistech.com>
2016-07-02 21:34:46 +0100

    Have a dropdown to choose which demo

--- src/cljs/nomisdraw/render.cljs
+++ src/cljs/nomisdraw/render.cljs
@@ -1,6 +1,7 @@
 (ns nomisdraw.render
   (:require [nomisdraw.play.re-com-slowness-play :as slowness-play]
             [nomisdraw.quil-play :as qp]
+            [reagent.core :as r]
             [re-com.core :as re]))
 
 (defn ^:private style-for-top-level-div []
@@ -8,12 +9,41 @@
     {:margin-left  m
      :margin-right m}))
 
+(def ^:private choices [{:id :some-quil-stuff
+                         :label "Some Quil Stuff"
+                         :fun qp/some-quil-stuff}
+                        {:id :nested-re-com-can-be-slow
+                         :label "Nested re-com can be slow"
+                         :fun slowness-play/make-re-com-stuff}])
+
+(def ^:private selected-demo-id (r/atom (-> choices
+                                            first
+                                            :id)))
+
+(defn ^:private the-choices []
+  [re/v-box
+   :width     "700px"
+   :gap       "10px"
+   :children  [[re/h-box
+                :gap      "10px"
+                :align    :center
+                :children [[re/label :label "Select a demo"]
+                           [re/single-dropdown
+                            :choices   choices
+                            :model     selected-demo-id
+                            :width     "300px"
+                            :on-change #(reset! selected-demo-id %)]]]
+               [re/gap :size "0px"] ;; Force a bit more space here
+               (let [fun (->> choices
+                              (filter #(= @selected-demo-id
+                                          (:id %)))
+                              first
+                              :fun)]
+                 [fun])]])
+
 (defn top-level-render []
   [re/v-box
    :style (style-for-top-level-div)
    :children
    [[:h1 "Nomisdraw"]
-    [qp/some-quil-stuff]
-    (case 1
-      1 nil
-      2 (slowness-play/make-re-com-stuff))]])
+    [the-choices]]])
