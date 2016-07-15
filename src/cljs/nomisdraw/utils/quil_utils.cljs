eb23ff8b2abbed463db0cb109a472be9cf3ed925
Simon Katz <sk@nomistech.com>
2016-07-16 00:06:55 +0100

    Refactor reusing-of-canvas-elements notes

--- src/cljs/nomisdraw/utils/quil_utils.cljs
+++ src/cljs/nomisdraw/utils/quil_utils.cljs
@@ -5,10 +5,22 @@
             [re-com.core :as re])
   (:require-macros [cljs.core.async.macros :as a]))
 
-;; Making Quil work well with Reagent...
-;; - This core idea comes from
-;;   http://stackoverflow.com/questions/33345084/quil-sketch-on-a-reagent-canvas
-;; - I've made it more functional.
+;;;; Making Quil work well with Reagent...
+;;;; - This core idea comes from
+;;;;   http://stackoverflow.com/questions/33345084/quil-sketch-on-a-reagent-canvas
+;;;; - I've made it more functional.
+
+;;;; TODO: reusing-of-canvas-elements
+;;;;       Investigate and maybe report bug.
+;;;;       When a canvas element is reused by a sketch (as will happen when
+;;;;       re-rendering happens (because of change of state or a Figwheel
+;;;;       reload)), Quil 2.4.0 is supposed to destroy the previous processing
+;;;;       object.
+;;;;       See commit https://github.com/quil/quil/commit/be03cdeb61e78ecf4d3e09a5fb8e8d3658631816.
+;;;        But I find the browser gets slower and slower and CPU usage
+;;;;       gets higher and higher. That's because the previous sketches' draw
+;;;;       functions continue to be called.
+;;;;       Hacky fix: wrap draw function in a check for being unmounted.
 
 (defn ^:private prevent-horizontal-stretching [elem]
   [re/h-box
@@ -28,7 +40,7 @@
                                      size))
         [w h]           size
         canvas-tag-&-id (keyword (str "canvas#" canvas-id))
-        unmounted?-atom (atom false)
+        unmounted?-atom (atom false) ; see reusing-of-canvas-elements
         draw            (:draw sketch-args)
         draw'           (fn [& args]
                           (when-not @unmounted?-atom
