d8348b048a249d3b5c44644b50edeb5fb7706cdc
Simon Katz <sk@nomistech.com>
2016-07-17 12:33:47 +0100

    Change order of demos in top-level menu

--- src/cljs/nomisdraw/top_level_render.cljs
+++ src/cljs/nomisdraw/top_level_render.cljs
@@ -16,12 +16,12 @@
    :style (style-for-top-level-div)
    :children
    [[:h1 "Nomisdraw"]
-    [dau/dropdown-and-chosen-item [{:id :some-quil-animation-stuff
-                                    :label "Some Quil Animation Stuff"
-                                    :fun #'qap/render}
-                                   {:id :quil-basics
+    [dau/dropdown-and-chosen-item [{:id :quil-basics
                                     :label "Examples from Quil Intro"
                                     :fun #'qi/render}
+                                   {:id :some-quil-animation-stuff
+                                    :label "Some Quil Animation Stuff"
+                                    :fun #'qap/render}
                                    {:id :nested-re-com-can-be-slow
                                     :label "Nested re-com can be slow"
                                     :fun #'slowness-play/render}]]]])
