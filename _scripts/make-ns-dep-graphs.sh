b6ef38efd5828e2b9badbe949462f874bf179203
Simon Katz <sk@nomistech.com>
2017-07-15 17:33:08 +0100

    Add `:write-gv-file?` to _scripts/make-ns-dep-graphs.sh

--- _scripts/make-ns-dep-graphs.sh
+++ _scripts/make-ns-dep-graphs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-lein nomis-ns-graph :platform clj  :filename nomis-ns-graph-clj :exclusions "user|midje"
-lein nomis-ns-graph :platform clj  :filename nomis-ns-graph-clj-with-externals :show-non-project-deps :exclusions "user|midje"
-lein nomis-ns-graph :platform cljs :filename nomis-ns-graph-cljs
-lein nomis-ns-graph :platform cljs :filename nomis-ns-graph-cljs-with-externals :show-non-project-deps
+lein nomis-ns-graph :write-gv-file? :platform clj  :filename nomis-ns-graph-clj :exclusions "user|midje"
+lein nomis-ns-graph :write-gv-file? :platform clj  :filename nomis-ns-graph-clj-with-externals :show-non-project-deps :exclusions "user|midje"
+lein nomis-ns-graph :write-gv-file? :platform cljs :filename nomis-ns-graph-cljs
+lein nomis-ns-graph :write-gv-file? :platform cljs :filename nomis-ns-graph-cljs-with-externals :show-non-project-deps
