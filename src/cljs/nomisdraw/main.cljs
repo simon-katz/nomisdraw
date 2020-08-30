1231942604c3371fd0044b55608ce45efc5548f8
Simon Katz <sk@nomistech.com>
2020-08-30 12:58:32 +0100

    Upgrade to reagent 0.10.0 and re-com 2.9.0

--- src/cljs/nomisdraw/main.cljs
+++ src/cljs/nomisdraw/main.cljs
@@ -1,13 +1,14 @@
 (ns nomisdraw.main
   (:require [nomisdraw.top-level-render :as top-level-render]
-            [reagent.core :as r]))
+            [reagent.dom :as dom]))
 
 (enable-console-print!)
 
 (println "Edits to this text should show up in your developer console.")
 
-(r/render-component [top-level-render/render]
-                    (. js/document (getElementById "app")))
+
+(dom/render [top-level-render/render]
+            (. js/document (getElementById "app")))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
